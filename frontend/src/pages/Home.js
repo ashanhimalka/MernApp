73e82554f43afe82528547f5ce61511e75cebe9a
ashanhimalka <ashan.rcm94@gmail.com>
2024-03-11 09:53:40 +0530

    Intial commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+
+// components
+import WorkoutDteails from '../components/WorkoutDetails'
+
+const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(()=> {
+        const fetchWorkouts  = async () => {
+            const response = await fetch("/api/workouts")
+            const json = await response.json()
+
+            if(response.ok) {
+                setWorkouts(json)
+            }
+        }
+        
+        fetchWorkouts()
+    }, [])
+
+    return (
+        <div className="home">
+            <div className='workouts'>
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDteails key= {workout._id} workout= {workout}/>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Home
